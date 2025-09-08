a5fe3f237380e2f0bb0d6db6fe36b4b184acd126
Amira khattab <amira17_6@hotmail.com>
2025-09-08 11:02:57 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+number1= float(input("please enter the first No: "))
+operator= input("Please enter the operator")
+number2= float(input("Please enter the second No: "))
+
+if operator== "+":
+    print (number1+number2)
+elif operator=="-":
+    print (number1-number2)
+elif operator=="/":
+    print (number1/number2)
+elif operator=="*":
+    print (number1*number2)
+else:
+    print ("owrong perator please try again")
